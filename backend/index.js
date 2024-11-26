b68de172b2dc0bf902b25bd943a26aa3ad858b45
Thushan W A R T <rusiththarindu2@gmail.com>
2024-11-26 20:10:00 +0530

    feat : CORS policy correction

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app =express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: 'https://dall-e-image-generator-frontend.vercel.app' }));
 app.use(express.json({limit: '50mb'}));
 
 app.use('/api/v1/post', postRoutes);
